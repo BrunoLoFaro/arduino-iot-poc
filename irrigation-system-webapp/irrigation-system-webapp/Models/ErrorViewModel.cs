e70f2594a16a27c2ec68ef19a6c628341ed771cf
BrunoLoFaro <lofarobruno@gmail.com>
2023-02-25 16:18:57 -0300

    initial commit

---
+++ irrigation-system-webapp/irrigation-system-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace irrigation_system_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
